e5e53268247cd03f2ce107bccb024f41fbd520ff
FARGO <fargo26sur@mail.ru>
2023-08-11 03:18:38 +0300

    idk

--- app/src/main/java/com/example/youtubedownloader/MainActivity.kt
+++ app/src/main/java/com/example/youtubedownloader/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.youtubedownloader
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -13,14 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (! Python.isStarted())
-            Python.start(AndroidPlatform(this))
+        if (!Python.isStarted()) Python.start(AndroidPlatform(this))
 
         val pythonObj = Python.getInstance()
         val module = pythonObj.getModule("downloader")
 
-        val str = module["main"]?.toString()
-        findViewById<TextView>(R.id.txtv).text = str
+
+        findViewById<Button>(R.id.mp4).setOnClickListener {
+            GlobalScope.launch {
+                module.callAttr(
+                    "download", "https://www.youtube.com/watch?v=ex9tML6udCU", filesDir.absolutePath
+                )
+            }
+
+        }
+
+//https://www.youtube.com/watch?v=ex9tML6udCU
 
     }
 }
--- app/src/main/python/downloader.py
+++ app/src/main/python/downloader.py
@@ -1,1 +1,10 @@
-main = "hello"
+from pytube import YouTube
+
+def download(link, path):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_lowest_resolution()
+    try:
+        youtubeObject.download(path)
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
