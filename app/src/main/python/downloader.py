78ebb5081f5f97ed791b1aab8ce16089690cbcff
FARGO <fargo26sur@mail.ru>
2023-08-19 21:46:14 +0300

    beta version

--- app/src/main/python/downloader.py
+++ app/src/main/python/downloader.py
@@ -12,18 +12,16 @@ def download(link, path):
 
     }
     with yt.YoutubeDL(ydl_opts) as ydl:
-        # info_dict = ydl.extract_info(link, download=False)
-        # video_title = str(info_dict.get('title', None))
+        info_dict = ydl.extract_info(link, download=False)
+        video_title = str(info_dict.get('title', None))
         ydl.download([link])
-    # return video_title
+        return video_title
 
 
 def convertToAudio(link, path):
     from com.arthenica.mobileffmpeg import FFmpeg
     from android.os import Environment
-    # title = download(link, path)
+    title = download(link, path)
     FFmpeg.execute("-i " + str(
-        Environment.getExternalStorageDirectory()) + "/Download/ytdl/Shrt.mp4" + " -vn -acodec libmp3lame -q:a 0 " + str(
-        Environment.getExternalStorageDirectory()) + "Sh.mp3")
-    print(str(
-        Environment.getExternalStorageDirectory()) + "/Download/ytdl/%(title)s.%(ext)s")
+        Environment.getExternalStorageDirectory()) + "/Download/ytdl/" + title + ".mp4" + " -vn -acodec libmp3lame -q:a 0 " + str(
+        Environment.getExternalStorageDirectory()) + "/Download/ytdl/" + title + ".mp3")
