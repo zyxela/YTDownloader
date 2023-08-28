5b42e0649f34c11bf8fb09c22e3826dc4558db1d
FARGO <fargo26sur@mail.ru>
2023-08-28 19:57:39 +0300

    transfer pb to viewmodel

--- app/src/main/java/com/example/youtubedownloader/MainActivity.kt
+++ app/src/main/java/com/example/youtubedownloader/MainActivity.kt
@@ -9,13 +9,12 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     lateinit var progressBar: ProgressBar
@@ -32,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         val module = pythonObj.getModule("downloader")
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         GlobalScope.launch {
-            updateProgress(module)
+            viewModel.updateProgress(module)
         }
         viewModel.currentProgress.observe(this) {
             progressBar.progress = viewModel.currentProgress.value ?: 0
@@ -41,16 +40,17 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.mp4).setOnClickListener {
             val link = findViewById<TextView>(R.id.tv_link).text.toString()
             Toast.makeText(this, "Start downloading", Toast.LENGTH_SHORT).show()
-
             try {
                 GlobalScope.launch {
                     module.callAttr("start", link, "video")
-                    Toast.makeText(applicationContext, "Downloaded", Toast.LENGTH_SHORT).show()
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(it.context, "Downloaded", Toast.LENGTH_SHORT).show()
+                    }
                 }
 
             } catch (e: Exception) {
                 Log.e("CONNECT", "${e.message}")
-                Toast.makeText(this, "INCORRECT LINK", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "${e.cause}", Toast.LENGTH_LONG).show()
             }
 
         }
@@ -60,24 +60,16 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Start downloading", Toast.LENGTH_SHORT).show()
                 GlobalScope.launch {
                     module.callAttr("start", link, "audio")
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(it.context, "Downloaded", Toast.LENGTH_SHORT).show()
+                    }
                 }
             } catch (e: Exception) {
-                Toast.makeText(this, "INCORRECT LINK", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "${e.cause}", Toast.LENGTH_LONG).show()
                 Log.i("e", e.message!!)
             }
 
         }
 
-    }
-
-    fun updateProgress(module: PyObject) = runBlocking {
-        val downloader = module["downloader"]
-
-        launch {
-            while (true) {
-                progressBar.progress = downloader!!.callAttr("return_progress_statement").toInt()
-                delay(10)
-            }
-        }
     }
 }
--- app/src/main/java/com/example/youtubedownloader/MainViewModel.kt
+++ app/src/main/java/com/example/youtubedownloader/MainViewModel.kt
@@ -2,6 +2,9 @@ package com.example.youtubedownloader
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.chaquo.python.PyObject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainViewModel : ViewModel() {
     val currentProgress: MutableLiveData<Int> by lazy {
@@ -12,6 +15,15 @@ class MainViewModel : ViewModel() {
         currentProgress.value = 0
     }
 
+    fun updateProgress(module: PyObject) = runBlocking {
+        val downloader = module["downloader"]
+
+        while (true) {
+            currentProgress.postValue(downloader?.callAttr("return_progress_statement")?.toInt())
+            delay(10)
+        }
+
+    }
 
 
 }
